4272709f5f0ef9a36922fcde0b0fe22e330b976b
bulatOFF <p.bulatov@ndmsystems.com>
2024-06-24 12:13:11 +0300

    Set test timeout to 400 s

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 300_000,
+  timeout: 400_000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: false,
