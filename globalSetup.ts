06d05eada64737f0342f1aef0d858904c42e34f9
Pavel Kostyurin <p.kostyurin@ndmsystems.com>
2024-06-26 15:17:16 +0300

    Do not request easyConfig from device

--- globalSetup.ts
+++ globalSetup.ts
@@ -1,16 +1,14 @@
 import dotenv from 'dotenv';
 import { writeFileSync } from 'fs';
 import { join } from 'path';
-import { requestEasyConfig } from './util/easyconfig';
 import { run } from './util/helperFunctions';
 
 export default async (): Promise<void> => {
   dotenv.config()
 
-  const ec = await requestEasyConfig()
-  const prettyEc = JSON.stringify(ec, null, 2);
-
-  writeFileSync(join(__dirname, 'util/welcome.json'), prettyEc)
+  // const ec = await requestEasyConfig()
+  // const prettyEc = JSON.stringify(ec, null, 2);
+  // writeFileSync(join(__dirname, 'util/welcome.json'), prettyEc)
 
   let dict = run(`curl ${process.env.HOST}/language/locale.en.json`)
   writeFileSync(join(__dirname, 'localization/device.en.json'), dict)
